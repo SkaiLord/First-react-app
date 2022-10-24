e0b43e1dc6c7d2617bbe7b047799ade6ff0e5ea7
SkaiLord <skailord007@gmail.com>
2022-10-24 17:39:38 +0530

    Added action slider

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,7 @@
-import moment from 'moment/moment'
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment/moment'
 
 export default function MainTable() {
 
@@ -18,8 +20,14 @@ export default function MainTable() {
         <span className="row-task">{capitalizeFirstLetter("my firfewwwwwwwwwwwwf wfw wvefef wfewfew wefwf wvfw fwew  est task")}</span>
     </div>
     <div className="row">
-        <span className="row-time">2:30 pm , 22 oct 2022</span>
-        <span className="row-task">{capitalizeFirstLetter("my fife wefwe vrst task")}</span>
+        <div className='content'>
+        <p className="row-time">2:30 pm , 22 oct 2022</p>
+        <p className="row-task">{capitalizeFirstLetter("my fife wefwe vrst task")}</p>
+        </div>
+        <div className='actions'>
+        <FontAwesomeIcon icon={faEdit} style={{cursor:"pointer"}}/>
+        <FontAwesomeIcon icon={faTrash}  style={{cursor:"pointer"}} />
+        </div>
     </div>
     <div className="row">
         <span className="row-time">2:30 pm , 22 oct 2022</span>
